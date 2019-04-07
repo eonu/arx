4898fdae5056f82f71af1f05f1e8c1dff7bea8b2
Edwin Onuonga <edwinonuonga@gmail.com>
2019-04-07 13:36:59 +0100

    Finish cleaner_spec.rb

--- spec/arx/cleaner_spec.rb
+++ spec/arx/cleaner_spec.rb
@@ -125,28 +125,94 @@ describe Cleaner do
     context 'with version' do
       context 'URL' do
         context 'old format' do
-
+          urls[:old].each_with_index do |url, i|
+            if i.even?
+              it "should extract an ID from #{url.inspect}" do
+                expect(subject.extract_id url, version: true).to eq 'cond-mat/9609089'
+              end
+            else
+              it "should extract an ID from #{url.inspect}" do
+                expect(subject.extract_id url, version: true).to eq 'cond-mat/9609089v1'
+              end
+            end
+          end
         end
         context 'new format' do
-
+          urls[:new].each_with_index do |url, i|
+            if i.even?
+              it "should extract an ID from #{url.inspect}" do
+                expect(subject.extract_id url, version: true).to eq '1105.5379'
+              end
+            else
+              it "should extract an ID from #{url.inspect}" do
+                expect(subject.extract_id url, version: true).to eq '1105.5379v1'
+              end
+            end
+          end
         end
       end
       context 'ID' do
         context 'old format' do
-            it { expect { subject.extract_version 'cond-mat/9609089' }.to raise_error ArgumentError }
-            it { expect(subject.extract_version 'cond-mat/9609089v1').to eq 1 }
-            it { expect { subject.extract_version 'cs/0003044' }.to raise_error ArgumentError }
-            it { expect(subject.extract_version 'cs/0003044v10').to eq 10 }
-            it { expect { subject.extract_version 'hep-th/9711200' }.to raise_error ArgumentError }
-            it { expect(subject.extract_version 'hep-th/9711200v100').to eq 100 }
+          it { expect(subject.extract_id 'cond-mat/9609089').to eq 'cond-mat/9609089' }
+          it { expect(subject.extract_id 'cond-mat/9609089v1').to eq 'cond-mat/9609089' }
+          it { expect(subject.extract_id 'cs/0003044').to eq 'cs/0003044' }
+          it { expect(subject.extract_id 'cs/0003044v10').to eq 'cs/0003044' }
+          it { expect(subject.extract_id 'hep-th/9711200').to eq 'hep-th/9711200' }
+          it { expect(subject.extract_id 'hep-th/9711200v100').to eq 'hep-th/9711200' }
         end
         context 'new format' do
-
+          it { expect(subject.extract_id '1105.5379').to eq '1105.5379' }
+          it { expect(subject.extract_id '1105.5379v1').to eq '1105.5379' }
+          it { expect(subject.extract_id '1710.02185').to eq '1710.02185' }
+          it { expect(subject.extract_id '1710.02185v10').to eq '1710.02185' }
+          it { expect(subject.extract_id '1703.04834').to eq '1703.04834' }
+          it { expect(subject.extract_id '1703.04834v100').to eq '1703.04834' }
         end
       end
     end
   end
   context '.extract_version' do
-
+    context 'URL' do
+      context 'old format' do
+        urls[:old].each_with_index do |url, i|
+          if i.even?
+            it { expect { subject.extract_version url }.to raise_error ArgumentError }
+          else
+            it "should extract a version number from #{url.inspect}" do
+              expect(subject.extract_version url).to eq 1
+            end
+          end
+        end
+      end
+      context 'new format' do
+        urls[:new].each_with_index do |url, i|
+          if i.even?
+            it { expect { subject.extract_version url }.to raise_error ArgumentError }
+          else
+            it "should extract a version number from #{url.inspect}" do
+              expect(subject.extract_version url).to eq 1
+            end
+          end
+        end
+      end
+    end
+    context 'ID' do
+      context 'old format' do
+        it { expect { subject.extract_version 'cond-mat/9609089' }.to raise_error ArgumentError }
+        it { expect(subject.extract_version 'cond-mat/9609089v1').to eq 1 }
+        it { expect { subject.extract_version 'cs/0003044' }.to raise_error ArgumentError }
+        it { expect(subject.extract_version 'cs/0003044v10').to eq 10 }
+        it { expect { subject.extract_version 'hep-th/9711200' }.to raise_error ArgumentError }
+        it { expect(subject.extract_version 'hep-th/9711200v100').to eq 100 }
+      end
+      context 'new format' do
+        it { expect { subject.extract_version '1105.5379' }.to raise_error ArgumentError }
+        it { expect(subject.extract_version '1105.5379v1').to eq 1 }
+        it { expect { subject.extract_version '1710.02185' }.to raise_error ArgumentError }
+        it { expect(subject.extract_version '1710.02185v10').to eq 10 }
+        it { expect { subject.extract_version '1703.04834' }.to raise_error ArgumentError }
+        it { expect(subject.extract_version '1703.04834v100').to eq 100 }
+      end
+    end
   end
 end
