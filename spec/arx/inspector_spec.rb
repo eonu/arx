debd1f70245a3a5c46fbb236807bf4ddaee7ff89
Edwin Onuonga <edwinonuonga@gmail.com>
2019-04-07 19:10:24 +0100

    Fix constant redefinition in Inspector specs

--- spec/arx/inspector_spec.rb
+++ spec/arx/inspector_spec.rb
@@ -1,54 +1,52 @@
 require 'spec_helper'
 
-describe Inspector do
-  subject do
-    Test = Class.new do
-      include Inspector
-      define_method(:a) { }
-      define_method(:b) { raise StandardError.new }
-      define_method(:c) { :c }
-      define_method(:d) { ?d.ord }
-      define_method(:e) { ?e }
-    end
-  end
+class Test
+  include Inspector
+  define_method(:a) { }
+  define_method(:b) { raise StandardError.new }
+  define_method(:c) { :c }
+  define_method(:d) { ?d.ord }
+  define_method(:e) { ?e }
+end
 
+describe Inspector do
   context 'no methods' do
-    before { subject.send :inspector }
+    before { Test.send :inspector }
 
     it do
-      instance = subject.new
+      instance = Test.new
       expect(instance.inspect).to eq "#<Test:#{instance.object_id} >"
     end
   end
   context 'erroneous methods' do
-    before { subject.send :inspector, :b }
+    before { Test.send :inspector, :b }
 
     it do
-      instance = subject.new
+      instance = Test.new
       expect(instance.inspect).to eq "#<Test:#{instance.object_id} >"
     end
   end
   context 'nil-return methods' do
-    before { subject.send :inspector, :a }
+    before { Test.send :inspector, :a }
 
     it do
-      instance = subject.new
+      instance = Test.new
       expect(instance.inspect).to eq "#<Test:#{instance.object_id} a=nil>"
     end
   end
   context 'valid-return methods' do
-    before { subject.send :inspector, *%i[c d e] }
+    before { Test.send :inspector, *%i[c d e] }
 
     it do
-      instance = subject.new
+      instance = Test.new
       expect(instance.inspect).to eq "#<Test:#{instance.object_id} c=:c, d=100, e=\"e\">"
     end
   end
   context 'all methods' do
-    before { subject.send :inspector, *%i[a b c d e] }
+    before { Test.send :inspector, *%i[a b c d e] }
 
     it do
-      instance = subject.new
+      instance = Test.new
       expect(instance.inspect).to eq "#<Test:#{instance.object_id} a=nil, c=:c, d=100, e=\"e\">"
     end
   end
