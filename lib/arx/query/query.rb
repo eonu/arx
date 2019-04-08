9d11c93c315489b1ec0592aa019b7e722f413e88
Edwin Onuonga <edwinonuonga@gmail.com>
2019-04-08 17:26:11 +0100

    Simplify Query#add_subquery

--- lib/arx/query/query.rb
+++ lib/arx/query/query.rb
@@ -199,12 +199,8 @@ module Arx
     # @param subquery [String] The subquery to add.
     def add_subquery(subquery)
       if search_query?
-        if ends_with_connective?
-          @query << "+#{subquery}"
-        else
-          add_connective :and
-          @query << "+#{subquery}"
-        end
+        add_connective :and unless ends_with_connective?
+        @query << "+#{subquery}"
       else
         @query << "&#{PARAMS[:search_query]}=#{subquery}"
       end
